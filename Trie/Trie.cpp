a4117261a05191bf57332a763b3adbc295415970
Fate Hardin <fhardin27@gmail.com>
2019-03-20 16:12:36 -0500

    Nice and Tidy

--- Trie/Trie.cpp
+++ Trie/Trie.cpp
@@ -3,7 +3,9 @@
 #include "pch.h"
 #include <sstream>
 #include <iostream>
+#include <vector>
 #include <string>
+#include <list>
 
 using namespace std;
 
@@ -20,11 +22,15 @@ private:
 	void show(Trie*);	//called by the print function, cycles through the trie to show everything off
 	void newBranch(string, Trie*, int, int);	//places new parts of the string into the trie
 	Trie *root;		//the root of the trie
+	bool findHelper(string, Trie*);		//helps search see if a node is in the trie
+	void findRHelper(string, list<string>, Trie*);	//helps findReplace determine what the new sentence will be
 
 public:
 	Trie();		//sets the root = nullptr to get the trie ready
 	void insert(string);	//inserts the given string into the parse tree
+	void findReplace(string, list<string>);
 	void print();	//calls show funtion
+	void search(string);	//checks to see if the given string is in the tree
 };
 
 Trie::Trie() {
@@ -39,7 +45,7 @@ void Trie::newBranch(string x, Trie* temp, int index, int dist) {
 			temp->kids[index]->isTerminal = true;
 		if (temp->kids[index]->words.length() != x.length()) {
 			return newBranch(x, temp->kids[index], 0, dist + 1);	//add 1 to the string size for the next node
-		}	
+		}
 	}
 }
 
@@ -53,7 +59,6 @@ int Trie::findDist(Trie* current, Trie* temp) {
 		}
 	}
 }
-
 
 void Trie::insert(string x) {
 	insertWork(x, root);
@@ -112,14 +117,84 @@ void Trie::show(Trie* temp) {
 	}
 }
 
+void Trie::search(string s) {
+	if (findHelper(s, root))
+		cout << "Found" << endl;
+	else
+		cout << "Not found" << endl;
+}
+
+bool Trie::findHelper(string s, Trie* temp) {
+	if (s != temp->words) {
+		for (int i = 0; i < 26; i++) {
+			if (temp->kids[i]) {
+				if (s.find(temp->kids[i]->words) != string::npos && s[0] == temp->kids[i]->words[0])
+					return findHelper(s, temp->kids[i]);
+			}
+			else
+				return false;
+		}
+	}
+	else if (s != "")
+		return true;
+	else
+		return false;
+}
+
 void Trie::print() {
 	cout << "Parent Node: Children" << endl;
 	show(root);
 }
 
+void Trie::findReplace(string s, list<string> dict) {
+	findRHelper(s, dict, root);
+}
+
+void Trie::findRHelper(string s, list<string> dict, Trie* temp) {
+	if (find(dict.begin(), dict.end(), temp->words) != dict.end())
+		cout << temp->words << " ";
+	else if (!temp->kids[0])
+		cout << temp->words << " ";
+	else
+		for (int i = 0; i < 26; i++) {
+			if (temp->kids[i]) {
+				if (s.find(temp->kids[i]->words) != string::npos && s[0] == temp->kids[i]->words[0])
+					return findRHelper(s, dict, temp->kids[i]);
+			}
+		}
+}
+
+void printSent(string sent, list<string>& dict) { //replaces the words in the sentence with they're substrings found in the dict by checking the tree
+	Trie tree;
+
+	istringstream iss(sent); //split everything over spaces and store it into s
+	vector<string> s;
+	string temp;
+	iss >> temp;
+	while (iss) {
+		s.push_back(temp);
+		iss >> temp;
+	}
+
+	for (auto const& value : s) {
+		tree.insert(value);
+	}
+
+	for (auto const& value : s) {
+		tree.findReplace(value, dict);
+	}
+
+	cout << endl;
+	tree.print();
+}
+
 int main()
 {
 	Trie tree;
+	//Testing inserting into the tree
+	cout << "================" << endl;
+	cout << "Basic Word Tree:" << endl;
+	cout << "================" << endl;
 	tree.insert("have");
 	tree.insert("seven");
 	tree.insert("cupcake");
@@ -129,12 +204,32 @@ int main()
 	tree.insert("caterpillar");
 	tree.insert("sleep");
 	tree.insert("rainbow");
-	tree.insert("1010");
-	tree.insert("0000");
-	tree.insert("1010");
-	tree.insert("0101");
-	tree.insert("010101");
 	tree.print();
-	string stop;
+	cout << "================" << endl;
+	cout << "Testing Search:" << endl;
+	cout << "================" << endl;
+	tree.search("cows");
+	tree.search("sleep");
+	tree.search("rain");
+	tree.search("");
+
+	Trie tree2;
+	cout << "================" << endl;
+	cout << "Testing for repeating (matrix example):" << endl;
+	cout << "================" << endl;
+	tree2.insert("1010");
+	tree2.insert("0000");
+	tree2.insert("1010");
+	tree2.insert("0101");
+	tree2.print();
+
+	cout << "================" << endl;
+	cout << "Creating a Trie from a sentence and replacing superstrings with given substrings in a list:" << endl;
+	cout << "================" << endl;
+	cout << "Original Sentence: the cattle was rattled by the battery" << endl;
+	list<string> z; z.push_back("cat"); z.push_back("bat"); z.push_back("rat"); z.push_back("t");
+	printSent("the cattle was rattled by the battery", z);
+	cout << "==========================" << endl;
+	string stop; //pls don't disapear oh mighty command window
 	cin >> stop;
 }
